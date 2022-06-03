397a725416f2780a53f526635c467f1f9788d26f
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-06-02 20:02:04 -0400

    imported vehicle and created a car class that extends vehicle

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,5 +1,6 @@
+
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle.js")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
